d111b67bdf8a478ef95063e36844f32ff42177e5
wangjiehang <7598202516@qq.com>
2017-03-22 13:41:27 +0800

    0322

--- Test/app/src/main/java/com/example/test/MainActivity.java
+++ Test/app/src/main/java/com/example/test/MainActivity.java
@@ -2,9 +2,10 @@ package com.example.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
